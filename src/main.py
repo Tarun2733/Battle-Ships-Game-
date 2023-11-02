74a3565c7761dad899c3d9c712257d0eabeaf892
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-11-02 09:15:19 +0530

    Imported the spaceships and transformed it to the right shap and size

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import pygame
+import os
 
 WIDTH , HEIGHT = 900 , 500
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Battle Ships")
 
-WHITE_COLOR = (255,255,255)
-FPS = 60
+WHITE_COLOR = (255,255,255) #setting the background color variable 
+FPS = 60                    # initializing the fps variable
 
+SpaceShip_Width , SpaceShip_Height = (55,40) #setting up width and height for the spaceship
+
+#Importing Yellow spaceship and transforming it to right orientation and size
+Yellow_SpaceShip_Image = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+Yellow_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Yellow_SpaceShip_Image, (SpaceShip_Width,SpaceShip_Height)), 90)
+
+
+#Importing the red spaceship and transforming it to the right orientation and size
+Red_SpaceShip_Image = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+Red_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Red_SpaceShip_Image, (SpaceShip_Width,SpaceShip_Height)), 270)
 
 def draw_window():
     WIN.fill(WHITE_COLOR)
+    WIN.blit(Yellow_SpaceShip, (300,100))
+    WIN.blit(Red_SpaceShip, (700,100))
     pygame.display.update()
 
 
