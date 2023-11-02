0c87ff14476cdce38d646d26a803a25792eb0e0e
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-11-02 10:45:26 +0530

    Arguments for draw function created

--- src/main.py
+++ src/main.py
@@ -20,14 +20,19 @@ Yellow_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Yellow_SpaceSh
 Red_SpaceShip_Image = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
 Red_SpaceShip = pygame.transform.rotate(pygame.transform.scale(Red_SpaceShip_Image, (SpaceShip_Width,SpaceShip_Height)), 270)
 
-def draw_window():
+def draw_window(red,yellow):
     WIN.fill(WHITE_COLOR)
-    WIN.blit(Yellow_SpaceShip, (300,100))
-    WIN.blit(Red_SpaceShip, (700,100))
+    WIN.blit(Yellow_SpaceShip, (yellow.x,yellow.y))
+    WIN.blit(Red_SpaceShip, (red.x,red.y))
     pygame.display.update()
 
 
 def main():
+    #<-------------making red and yellow arguments that will be passed in the function-------------------->
+    red = pygame.Rect(700,300,SpaceShip_Width,SpaceShip_Height)
+    yellow = pygame.Rect(100,300,SpaceShip_Width,SpaceShip_Height)
+
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -35,7 +40,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        red.x -= 1                              # moving the red spaceship by 1 pixel 
+        draw_window(red,yellow)
+    
 
 if __name__ == '__main__':
     main()
