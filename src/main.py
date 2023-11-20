50dc473e269a1d45c71a22aa9fe430dbd52c1692
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-11-20 16:12:28 +0530

    The sound effects of the shots are also added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 pygame.font.init()
+pygame.mixer.init()
 
 
 WIDTH , HEIGHT = 900 , 500
@@ -18,6 +19,9 @@ FPS = 60                                                    # initializing the f
 BORDER = pygame.Rect(WIDTH//2 - 5 , 0 , 10 , HEIGHT)        # creating a rectangle in the middle to section the window
 Bullet_vel = 7
 MAX_BULLETS = 3
+
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
 
 
 YELLOW_HIT = pygame.USEREVENT + 1
@@ -140,16 +144,20 @@ def main():
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2,10,5)
                     yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
                 
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x, red.y + red.height//2 - 2,10,5)
                     red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
             if event.type == RED_HIT:
                 red_health -= 1
+                BULLET_HIT_SOUND.play()
 
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
+                BULLET_HIT_SOUND.play()
 
         winner_text =""
         if red_health <= 0:
